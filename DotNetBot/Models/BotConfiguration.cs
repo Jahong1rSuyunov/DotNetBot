a8d1c680cc2680b717919c44fc05ebc5597f54a4
jahongirsuyunov836@gmail.com <jahongirsuyunov836@gmail.com>
2022-03-15 14:37:47 +0300

    Add project files.

---
+++ DotNetBot/Models/BotConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoDotnetBot.Models
+{
+    public class BotConfiguration
+    {
+        public string Token { get; set; }
+        public string HostAdress { get; set; }
+    }
+}
